526e0e0c7bfe50edbe3c0182e43b359c94334b81
Thanaphat Suwannikornkul <onsensei@hotmail.com>
2018-01-03 19:56:31 +0700

    modular func

--- app/index.js
+++ app/index.js
@@ -21,51 +21,92 @@ export default class App extends Component {
     android: View
   });
 
+  Operator = {
+    Add: '+',
+    Subtract: '-',
+    Multiple: '*',
+    Divide: '/'
+  };
+
+  TextInputId = {
+    Operand1: 'Operand 1',
+    Operand2: 'Operand 2'
+  };
+
+  // ----------
+  
   state = {
     operInputValue1: '0',
     operInputValue2: '0',
     resultTitle: '0'
   };
 
+  // ----------
+  
+  receiveNumberInput = (id, text) => {
+    const result = text.replace(/[^0-9]/g, '');
+    switch (id) {
+    case this.TextInputId.Operand1:
+      this.setState({
+        operInputValue1: result
+      });
+      break;
+
+    case this.TextInputId.Operand2:
+      this.setState({
+        operInputValue2: result
+      });
+      break;
+    
+    default:
+      break;
+    }
+  }
+
+  calculate = (operator) => () => {
+    let result = null;
+    const value1 = parseFloat(this.state.operInputValue1);
+    const value2 = parseFloat(this.state.operInputValue2);
+
+    switch (operator) {
+    case this.Operator.Add:
+      result = value1 + value2;
+      break;
+
+    case this.Operator.Subtract:
+      result = value1 - value2;
+      break;
+
+    case this.Operator.Multiple:
+      result = value1 * value2;
+      break;
+
+    case this.Operator.Divide:
+      result = value1 / value2;
+      break;
+    
+    default:
+      break;
+    }
+
+    if (result) {
+      this.setState({
+        resultTitle: result.toString()
+      });
+    }
+  }
+
+  // ----------
+  
   onOperInput1ChangeText = (text) => {
-    this.setState({
-      operInputValue1: text.replace(/[^0-9]/g, '')
-    });
+    this.receiveNumberInput(this.TextInputId.Operand1, text);
   }
 
   onOperInput2ChangeText = (text) => {
-    this.setState({
-      operInputValue2: text.replace(/[^0-9]/g, '')
-    });
+    this.receiveNumberInput(this.TextInputId.Operand2, text);
   }
 
-  onAddButtonPress = () => {
-    const result = parseFloat(this.state.operInputValue1) + parseFloat(this.state.operInputValue2);
-    this.setState({
-      resultTitle: result.toString()
-    });
-  }
-
-  onSubtractButtonPress = () => {
-    const result = parseFloat(this.state.operInputValue1) - parseFloat(this.state.operInputValue2);
-    this.setState({
-      resultTitle: result.toString()
-    });
-  }
-
-  onMultipleButtonPress = () => {
-    const result = parseFloat(this.state.operInputValue1) * parseFloat(this.state.operInputValue2);
-    this.setState({
-      resultTitle: result.toString()
-    });
-  }
-
-  onDivideButtonPress = () => {
-    const result = parseFloat(this.state.operInputValue1) / parseFloat(this.state.operInputValue2);
-    this.setState({
-      resultTitle: result.toString()
-    });
-  }
+  // ----------
 
   render () {
     return (
@@ -73,14 +114,14 @@ export default class App extends Component {
         style={styles.container}
         behavior='padding'>
         <View style={styles.operInputContainer}>
-          <OperInput title='Operand 1' value={this.state.operInputValue1} onChangeText={this.onOperInput1ChangeText}/>
-          <OperInput title='Operand 2' value={this.state.operInputValue2} onChangeText={this.onOperInput2ChangeText}/>
+          <OperInput title={this.TextInputId.Operand1} value={this.state.operInputValue1} onChangeText={this.onOperInput1ChangeText}/>
+          <OperInput title={this.TextInputId.Operand2} value={this.state.operInputValue2} onChangeText={this.onOperInput2ChangeText}/>
         </View>
         <View style={styles.operButtonContainer}>
-          <OperButton title='+' onButtonPress={this.onAddButtonPress}/>
-          <OperButton title='-' onButtonPress={this.onSubtractButtonPress}/>
-          <OperButton title='X' onButtonPress={this.onMultipleButtonPress}/>
-          <OperButton title='/' onButtonPress={this.onDivideButtonPress}/>
+          <OperButton title={this.Operator.Add} onButtonPress={this.calculate(this.Operator.Add)}/>
+          <OperButton title={this.Operator.Subtract} onButtonPress={this.calculate(this.Operator.Subtract)}/>
+          <OperButton title={this.Operator.Multiple} onButtonPress={this.calculate(this.Operator.Multiple)}/>
+          <OperButton title={this.Operator.Divide} onButtonPress={this.calculate(this.Operator.Divide)}/>
         </View>
         <View style={styles.resultContainer}>
           <Result title={this.state.resultTitle}/>
